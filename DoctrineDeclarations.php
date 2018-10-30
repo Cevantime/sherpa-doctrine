9647dc603ec771cf669e78e3e991730c9c5d1b99
Thibault Truffert <thibaulttruffert@hotmail.com>
2018-10-30 22:32:36 +0100

    refacto

--- DoctrineDeclarations.php
+++ DoctrineDeclarations.php
@@ -3,6 +3,7 @@
 namespace Sherpa\Doctrine;
 
 use DI\Container;
+use function DI\env;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Tools\Setup;
@@ -23,21 +24,23 @@ class DoctrineDeclarations implements DeclarationInterface
         $containerBuilder = $app->getContainerBuilder();
 
         $containerBuilder->addDefinitions([
-            'doctrine.config' => function(Container $container) {
-                return Setup::createAnnotationMetadataConfiguration([$container->get('projectSrc') . "/Entity"], $container->get('debug'));
+            'doctrine.config' => function (Container $container) {
+                return Setup::createAnnotationMetadataConfiguration([$container->get('project.src') . "/Entity"], $container->get('debug'));
             },
-            EntityManagerInterface::class => function(Container $container) {
+            EntityManagerInterface::class => function (Container $container) {
                 return EntityManager::create(
-                    $container->get('doctrine.connection'), 
+                    $container->get('doctrine.connection'),
                     $container->get('doctrine.config')
                 );
             },
+            'doctrine.entity_folders' => function (Container $container) {
+                [$container->get('project.src') . "/Entity"];
+            },
             'doctrine.manager' => get(EntityManagerInterface::class),
-            'doctrine.connection' => function(Container $container) {
-                $originalRequest = $container->get('original_request');
-                $serverParams = $originalRequest->getServerParams();
-                return ['url' => $serverParams['DATABASE_URL'] ?? ''];
-            }
+            'doctrine.connection' => function (Container $container) {
+                return ['url' => $container->get('doctrine.connection.url')];
+            },
+            'doctrine.connection.url' => env('DATABASE_URL')
         ]);
     }
 
